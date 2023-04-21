03c257dc5e2e2aa30df23765dc2c2a3427f54649
diablo51030 <116533991+diablo51030@users.noreply.github.com>
2023-04-21 18:20:49 +0800

    Add files via upload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,8 @@ export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss','@pinia/nuxt'],
     typescript: {
         typeCheck: true
-    } 
+    },
+    runtimeConfig:{
+        jwtSignSecret:'TEST'
+    }
 })
